4296580e474c78ab603dba8251c0d99709231fab
manyaagarwal <manya18ag@gmail.com>
2020-06-03 04:02:17 +0530

    switch expo to minimal with react-native-paper

---
+++ mobile/constants/index.js
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'react-native-paper';
+
+export const appTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: 'cyan',
+      accent: 'black',
+    },
+  };
+
+export const lightTheme = {};
