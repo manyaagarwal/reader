c99a82324ecaed13642bc3886e8651633083c848
E-Liang Tan <eliang@eliangtan.com>
2020-06-03 00:12:11 +0800

    Implement business logic layer
    - CRUD functions for books
    - Books exposed to GraphQL have a globally unique ID to conform to the
      Relay Node interface (which we do not yet have)

--- server/src/book.ts
+++ server/src/book.ts
@@ -1,3 +1,9 @@
+import { toGlobalId, fromGlobalId } from "graphql-relay";
+import db from "./db";
+
+/**
+ * A book record, as stored by the database.
+ */
 interface Book {
   id: string;
   name?: string;
@@ -6,25 +12,96 @@ interface Book {
   lastReadAt?: Date;
 }
 
-type CreateBookInput = Omit<Book, "id">;
-type UpdateBookInput = Pick<Book, "id"> & Partial<Omit<Book, "id">>;
+/**
+ * The book record exposed to our GraphQL clients.
+ */
+type ExternalBook = Omit<Book, "lastReadAt"> & {
+  lastReadAt?: string;
+};
 
-export function getAllBooks(): Book[] {
-  // TODO:
-  return [];
+type CreateBookInput = Omit<ExternalBook, "id">;
+type UpdateBookInput = Pick<ExternalBook, "id"> &
+  Partial<Omit<ExternalBook, "id">>;
+
+// Internal/external transformers
+
+/**
+ * Book type identifier. Will be embedded in Relay global IDs.
+ */
+const BOOK_TYPE = "Book";
+
+/**
+ * If the Relay global ID is a book ID, returns the book's local ID. Throws
+ * otherwise.
+ * @param globalId GraphQL layer global ID.
+ */
+function fromGlobalIdOrThrow(globalId: string): string {
+  const localId = fromGlobalId(globalId);
+  if (localId.type !== BOOK_TYPE) {
+    throw new Error("ID must be a Book ID!");
+  }
+  return localId.id;
 }
 
-export function createBook(input: CreateBookInput): Book {
-  // TODO:
-  return { id: "stub" };
+function transformToExternalBook(book: Book): ExternalBook {
+  return {
+    ...book,
+    id: toGlobalId(BOOK_TYPE, book.id),
+    lastReadAt: book.lastReadAt?.toISOString(),
+  };
 }
 
-export function updateBook(input: UpdateBookInput): Book {
-  // TODO:
-  return { id: "stub" };
+function transformCreateInputToInternalBook(
+  input: CreateBookInput
+): Omit<Book, "id"> {
+  return {
+    ...input,
+    lastReadAt: input.lastReadAt ? new Date(input.lastReadAt) : undefined,
+  };
 }
 
-export function deleteBook(id: string): Book {
-  // TODO:
-  return { id: "stub" };
+function transformUpdateInputToInternalBook(input: UpdateBookInput): Book {
+  return {
+    ...input,
+    id: fromGlobalIdOrThrow(input.id),
+    lastReadAt: input.lastReadAt ? new Date(input.lastReadAt) : undefined,
+  };
+}
+
+// Book CRUD
+
+/**
+ * A collection of books. Exists so that lodash-id will automatically set IDs
+ * for us.
+ */
+const bookCollection = db.defaults({ books: [] }).get("books");
+
+export function getAllBooks(): ExternalBook[] {
+  const books: Book[] = bookCollection.value();
+  return books.map((book) => transformToExternalBook(book));
+}
+
+export function createBook(input: CreateBookInput): ExternalBook {
+  // TODO: Fix typings
+  const book: Book = (bookCollection as any)
+    .insert(transformCreateInputToInternalBook(input))
+    .write();
+  return transformToExternalBook(book);
+}
+
+export function updateBook(input: UpdateBookInput): ExternalBook | undefined {
+  const bookToBeSaved = transformUpdateInputToInternalBook(input);
+  // TODO: Fix typings
+  const book: Book | undefined = (bookCollection as any)
+    .updateById(bookToBeSaved.id, bookToBeSaved)
+    .write();
+  return book ? transformToExternalBook(book) : undefined;
+}
+
+export function deleteBook(id: string): ExternalBook | undefined {
+  // TODO: Fix typings
+  const book: Book | undefined = (bookCollection as any)
+    .removeById(fromGlobalIdOrThrow(id))
+    .write();
+  return book ? transformToExternalBook(book) : undefined;
 }
