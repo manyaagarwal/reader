0e6df0293b9226b5b8d98bd1bb07b08d561fb558
chirag-singhal <csinghal208@gmail.com>
2020-06-04 00:20:13 +0530

    prettier formatting

--- mobile/screens/AddBook.tsx
+++ mobile/screens/AddBook.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
-import { Button, Overlay } from 'react-native-elements';
+import React, { useState } from "react";
+import { Button, Overlay } from "react-native-elements";
 import { View, Text } from "react-native";
-import { StyleSheet, Image, TouchableOpacity, Alert } from 'react-native';
-import AddBookForm from "./AddBookForm"
+import { StyleSheet, Image, TouchableOpacity, Alert } from "react-native";
+import AddBookForm from "./AddBookForm";
 
 export default function AddBook() {
-
   const [visible, setVisible] = useState(false);
 
   const toggleOverlay = () => {
@@ -14,34 +13,39 @@ export default function AddBook() {
 
   return (
     <View style={styles.MainContainer}>
-        <TouchableOpacity activeOpacity={0.5} onPress={toggleOverlay}>
-          <Image source={{uri : 'https://reactnativecode.com/wp-content/uploads/2017/11/Floating_Button.png'}} 
-                onPress={toggleOverlay} style={styles.FloatingButtonStyle} />
-        </TouchableOpacity>
-      <Overlay isVisible={visible} onBackdropPress={toggleOverlay} style ={{}}>
+      <TouchableOpacity activeOpacity={0.5} onPress={toggleOverlay}>
+        <Image
+          source={{
+            uri:
+              "https://reactnativecode.com/wp-content/uploads/2017/11/Floating_Button.png"
+          }}
+          onPress={toggleOverlay}
+          style={styles.FloatingButtonStyle}
+        />
+      </TouchableOpacity>
+      <Overlay isVisible={visible} onBackdropPress={toggleOverlay} style={{}}>
         <AddBookForm />
       </Overlay>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
+  MainContainer: {
+    position: "absolute",
+    right: 30,
+    bottom: 30,
+    backgroundColor: "#F5F5F5"
+  },
 
-    MainContainer: {
-      position: 'absolute',
-      right: 30,
-      bottom: 30,
-      backgroundColor : '#F5F5F5'
-    },
-  
-    TouchableOpacityStyle:{
-      width: 50,
-      height: 50
-    },
-  
-    FloatingButtonStyle: {
-      resizeMode: 'contain',
-      width: 50,
-      height: 50,
-    }
-  });
+  TouchableOpacityStyle: {
+    width: 50,
+    height: 50
+  },
+
+  FloatingButtonStyle: {
+    resizeMode: "contain",
+    width: 50,
+    height: 50
+  }
+});
--- mobile/screens/AddBookForm.tsx
+++ mobile/screens/AddBookForm.tsx
@@ -1,29 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { Input, Button } from 'react-native-elements';
-import { StyleSheet, View } from 'react-native';
+import { Input, Button } from "react-native-elements";
+import { StyleSheet, View } from "react-native";
 
 export default class AddBookForm extends Component<{}> {
-
   render() {
     return (
       <View style={styles.MainContainer}>
-          <Input placeholder="Book Name" onChangeText={value => this.setState({ bookName: value })} style = {styles.Input} /> 
-          <Input placeholder="Total Number Of Pages" onChangeText={value => this.setState({ total: value })} style = {styles.Input} />
-          <Input placeholder="Number Of Pages Read" onChangeText={value => this.setState({ read: value })} style = {styles.Input} />
-          <Button title="Add Book" />
+        <Input
+          placeholder="Book Name"
+          onChangeText={value => this.setState({ bookName: value })}
+          style={styles.Input}
+        />
+        <Input
+          placeholder="Total Number Of Pages"
+          onChangeText={value => this.setState({ total: value })}
+          style={styles.Input}
+        />
+        <Input
+          placeholder="Number Of Pages Read"
+          onChangeText={value => this.setState({ read: value })}
+          style={styles.Input}
+        />
+        <Button title="Add Book" />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-
   MainContainer: {
-      height: 300,
-      width: 300
+    height: 300,
+    width: 300
   },
   Input: {
-      marginTop: 20
+    marginTop: 20
   }
 });
