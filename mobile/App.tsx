c37daaae827e53f77c286fd2a1ef3430a58ff32b
E-Liang Tan <eliang@eliangtan.com>
2020-06-03 13:54:45 +0800

    Add Prettier to /mobile and format all files

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Provider as PaperProvider } from "react-native-paper";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -15,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- mobile/constants/index.js
+++ mobile/constants/index.js
@@ -1,12 +1,12 @@
-import { DefaultTheme } from 'react-native-paper';
+import { DefaultTheme } from "react-native-paper";
 
 export const appTheme = {
-    ...DefaultTheme,
-    colors: {
-      ...DefaultTheme.colors,
-      primary: 'cyan',
-      accent: 'black',
-    },
-  };
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "cyan",
+    accent: "black",
+  },
+};
 
 export const lightTheme = {};
