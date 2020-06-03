5f990bd83aa3fa5acaad3b3291ae1f496cd7c3f9
E-Liang Tan <eliang@eliangtan.com>
2020-06-03 20:58:10 +0800

    Prettify components folder

--- mobile/components/BookCard.tsx
+++ mobile/components/BookCard.tsx
@@ -1,15 +1,16 @@
 import * as React from "react";
-import {Card, Paragraph} from "react-native-paper";
+import { Card, Paragraph } from "react-native-paper";
 
 // @ts-ignore
-export function BookCard({key,book}) {
-    return (
-        <Card key={key}>
-            <Card.Title title={book.name} />
-            <Card.Content>
-                <Paragraph>{book.currentPageNum} / {book.numPages}</Paragraph>
-            </Card.Content>
-        </Card>
-    );
+export function BookCard({ key, book }) {
+  return (
+    <Card key={key}>
+      <Card.Title title={book.name} />
+      <Card.Content>
+        <Paragraph>
+          {book.currentPageNum} / {book.numPages}
+        </Paragraph>
+      </Card.Content>
+    </Card>
+  );
 }
-
