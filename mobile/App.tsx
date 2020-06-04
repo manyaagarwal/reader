ddd48be04597472bc344f50decaa2cdb8c46737e
manyaagarwal <manya18ag@gmail.com>
2020-06-04 11:47:22 +0530

    relay api

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import Navigation from "./Navigation";
-import { appTheme } from "./constants";
 
 export default function App() {
   return (
-    <PaperProvider theme={appTheme}>
-      <StatusBar backgroundColor={appTheme.colors.primary} />
+    <PaperProvider>
       <Navigation />
     </PaperProvider>
   );
