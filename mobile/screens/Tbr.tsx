73e89414bac3bac64552753c3a6ab849b81a4bea
E-Liang Tan <eliang@eliangtan.com>
2020-06-04 17:44:26 +0800

    Format code

--- mobile/screens/Tbr.tsx
+++ mobile/screens/Tbr.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
-import {BookListPage} from "../components/BookListPage";
+import { BookListPage } from "../components/BookListPage";
 
 export function ToBeRead() {
-  return (
-      <BookListPage />
-  );
+  return <BookListPage />;
 }
