1efedd603b94127bbd6c42befac05b24cf543590
manyaagarwal <manya18ag@gmail.com>
2020-06-04 15:10:56 +0530

    add constants file

---
+++ mobile/constants/index.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "react-native-paper";
+
+export const appTheme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: "#009fb7ff",
+        accent: "#fed766ff",
+        background: "#eff1f3ff",
+        text: "#272727ff",
+        disabled: "#696773ff",
+    },
+};
