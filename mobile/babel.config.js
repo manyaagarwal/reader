39098fe503378ce92b42ea978d9de6ec126ffad3
chirag-singhal <csinghal208@gmail.com>
2020-06-04 13:28:15 +0530

    format code

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,13 +1,14 @@
-module.exports = function(api) {
-    api.cache(true);
-    return {
-        presets: ["babel-preset-expo"],
-        env: {
-            production: {
-                plugins: [
-                    ["relay", { "schema": "data/schema.json" }], "react-native-paper/babel"
-                ],
-            },
-        },
-    };
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    env: {
+      production: {
+        plugins: [
+          ["relay", { schema: "data/schema.json" }],
+          "react-native-paper/babel",
+        ],
+      },
+    },
+  };
 };
